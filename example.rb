ae13756a54b3333ee25f4e3e0597b084a1763ec5
Whapow <joshuavancamp+github@gmail.com>
2017-03-06 15:24:38 -0500

    Allow example.rb to run without @config

--- example.rb
+++ example.rb
@@ -8,7 +8,7 @@ end
 Dir.glob('*').each do |folder_name|
   Dir.chdir(folder_name) do
     Dir.glob("*.csv") do |file|
-      if process(File.expand_path(file),@config.server,@config.language)
+      if process(File.expand_path(file))
         FileUtils.move(file, "../../Processed/#{folder_name}/")
       else
         # system('ruby', @config.failure_path, File.expand_path(file), @config.server, @config.language)
